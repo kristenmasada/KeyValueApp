ba335601fde5797d55a4db1f91983217f63c3dab
Kristen Masada <km942412@ohio.edu>
2015-08-06 16:09:32 -0400

    Initial Commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeyValueApp
+//
+//  Created by Kristen Masada on 8/6/15.
+//  Copyright (c) 2015 Nice Mohawk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

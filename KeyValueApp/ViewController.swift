2c70e60c18b4527ea0be72f6b8483fc353120863
Kristen Masada <km942412@ohio.edu>
2015-08-07 13:43:37 -0400

    iCloud integration.

--- KeyValueApp/ViewController.swift
+++ KeyValueApp/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var keyStore: NSUbiquitousKeyValueStore?
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        keyStore = NSUbiquitousKeyValueStore()
+ 
+        if let savedValue = keyStore?.stringForKey("Text") {
+            textField.text = savedValue
+        }
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "KeyValueStoreDidChange:", name: NSUbiquitousKeyValueStoreDidChangeExternallyNotification, object: keyStore)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func KeyValueStoreDidChange(notification: NSNotification) {
+        textField.text = keyStore?.stringForKey("Text")
+    }
 
+    @IBAction func saveToIcloud() {
+        keyStore?.setString(textField.text, forKey: "Text")
+        keyStore?.synchronize()
+    }
 
 }
 
